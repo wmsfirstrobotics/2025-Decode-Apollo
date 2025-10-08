a94a21c336167fd8d2345a0204970fb6aae02498
dockterj <dockterj@gmail.com>
2025-10-07 20:00:22 -0400

    Code push

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagLimelightTest.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+import java.util.List;
+
+@TeleOp(name = "AprilTag Limelight Test", group = "Testing")
+public class AprilTagLimelightTest extends OpMode {
+    private Limelight3A limelight;
+    private IMU imu;
+
+    @Override
+    public void init() {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.pipelineSwitch(0); // AprilTag pipeline
+
+        imu = hardwareMap.get(IMU.class, "imu");
+        RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
+        );
+        imu.initialize(new IMU.Parameters(revHubOrientationOnRobot));
+    }
+
+    @Override
+    public void start() {
+        limelight.start();
+    }
+
+    @Override
+    public void loop() {
+        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+        LLResult llResult = limelight.getLatestResult();
+
+        if (llResult != null && llResult.isValid()) {
+            Pose3D botpose = llResult.getBotpose_MT2();
+            telemetry.addData("Bot Pose (MT2)", botpose);
+
+            // Access fiducial results
+            List<LLResultTypes.FiducialResult> fiducialResults = llResult.getFiducialResults();
+            for (LLResultTypes.FiducialResult fr : fiducialResults) {
+                telemetry.addData("AprilTag ID", fr.getFiducialId());
+                telemetry.addData("Target X°", fr.getTargetXDegrees());
+                telemetry.addData("Target Y°", fr.getTargetYDegrees());
+            }
+        } else {
+            telemetry.addData("Limelight", "No valid target");
+        }
+
+        telemetry.update();
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
@@ -236,8 +236,8 @@ public final class TankDrive {
         // TODO: make sure your config has motors with these names (or change them)
         //   add additional motors on each side if you have them
         //   see https://ftc-docs.firstinspires.org/en/latest/hardware_and_software_configuration/configuring/index.html
-        leftMotors = Arrays.asList(hardwareMap.get(DcMotorEx.class, "left"));
-        rightMotors = Arrays.asList(hardwareMap.get(DcMotorEx.class, "right"));
+        leftMotors = Arrays.asList(hardwareMap.get(DcMotorEx.class, "leftmotor"));
+        rightMotors = Arrays.asList(hardwareMap.get(DcMotorEx.class, "rightmotor"));
 
         for (DcMotorEx m : leftMotors) {
             m.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
