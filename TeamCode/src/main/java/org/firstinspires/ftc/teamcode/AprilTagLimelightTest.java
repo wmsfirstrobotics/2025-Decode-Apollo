d5c0daa0b9c2867dd618487146704040e854e0d4
mason g <masong1056@gmail.com>
2025-10-09 19:52:24 -0400

    Added test to see if GitHub and Android Studio are working on my MacBook.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagLimelightTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagLimelightTest.java
@@ -1,5 +1,4 @@
 //this is a test to see if github android studio is working on mason's macbook
-//test2
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.limelightvision.LLResult;
