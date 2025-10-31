2396345ff8abe75d063391da1cef7f40f45f3cb4
mason g <masong1056@gmail.com>
2025-10-30 20:16:16 -0400

    got roadrunner ACTUALLY working
    in other terms:
    - meep meep added (i forgot)
    - uploaded decode map for mm

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,24 +1,20 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.Rotation2d;
-import com.acmerobotics.roadrunner.Action;
-import com.acmerobotics.roadrunner.SleepAction;
 import com.noahbres.meepmeep.MeepMeep;
-import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
-import com.noahbres.meepmeep.roadrunner.DriveTrainType;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(650);
+        MeepMeep meepMeep = new MeepMeep(800);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .setDimensions(15,15)
-                .setDriveTrainType(DriveTrainType.TANK)
                 .build();
 
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
@@ -32,7 +28,15 @@ public class MeepMeepTesting {
                 .turn(Math.toRadians(90))
                 .build());
 
-        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_OFFICIAL)
+        Image customBackground = null;
+        try {
+            customBackground = ImageIO.read(new File("Decode_Map_Dark.png"));
+        } catch (IOException e) {
+            meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL);
+        }
+
+
+        meepMeep.setBackground(customBackground)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
