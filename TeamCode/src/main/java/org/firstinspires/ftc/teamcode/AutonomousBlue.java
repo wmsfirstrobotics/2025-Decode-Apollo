4d5bb20798c5afe12df08729d66eb8cb356a4dda
mason g <masong1056@gmail.com>
2025-11-20 20:00:04 -0500

    auton stuff part 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBlue.java
@@ -1,26 +1,39 @@
 package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public class AutonomousBlue {
-    public static void main(String[] args) {
-        MecanumDrive myBot = new MecanumDrive();
+@Autonomous(name = "AutonFirstTest", group = "Autonomous")
+public class AutonomousBlue extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        MecanumDrive myBot = new MecanumDrive(hardwareMap, new Pose2d(-61, -33, Math.toRadians(0)));
 
+        TrajectoryActionBuilder trajectory = myBot.actionBuilder(new Pose2d(-61, -33, Math.toRadians(0)))
+                .waitSeconds(2)
+                .lineToX(22)
+                .turn(Math.toRadians(90));
 
-        myBot.runAction(myBot.actionBuilder(new Pose2d(-61, -33, Math.toRadians(0)))
-                .waitSeconds(2)
-                .splineToLinearHeading(new Pose2d(-11.5, -27, Math.toRadians(270)), Math.toRadians(270))
-                .lineToY(-47.5)
-                .splineToLinearHeading(new Pose2d(-23.5, -23.5, Math.toRadians(54)), Math.toRadians(-200))
-                .waitSeconds(2)
-                .splineToLinearHeading(new Pose2d(11.5, -27, Math.toRadians(270)), Math.toRadians(270))
-                .lineToY(-47.5)
-                .splineToLinearHeading(new Pose2d(-23.5, -23.5, Math.toRadians(54)), Math.toRadians(-200))
-                .waitSeconds(2)
-                .splineToLinearHeading(new Pose2d(35.5, -27, Math.toRadians(270)), Math.toRadians(270))
-                .lineToY(-47.5)
-                .splineToLinearHeading(new Pose2d(-23.5, -23.5, Math.toRadians(54)), Math.toRadians(-200))
-                .waitSeconds(2)
-                .build());
+//                .lineToY(-47.5)
+//                .splineToLinearHeading(new Pose2d(-23.5, -23.5, Math.toRadians(54)), Math.toRadians(-200))
+//                .waitSeconds(2)
+//                .splineToLinearHeading(new Pose2d(11.5, -27, Math.toRadians(270)), Math.toRadians(270))
+//                .lineToY(-47.5)
+//                .splineToLinearHeading(new Pose2d(-23.5, -23.5, Math.toRadians(54)), Math.toRadians(-200))
+//                .waitSeconds(2)
+//                .splineToLinearHeading(new Pose2d(35.5, -27, Math.toRadians(270)), Math.toRadians(270))
+//                .lineToY(-47.5)
+//                .splineToLinearHeading(new Pose2d(-23.5, -23.5, Math.toRadians(54)), Math.toRadians(-200))
+//                .waitSeconds(2);
 
+//        TrajectoryActionBuilder trajectory = myBot.actionBuilder(new Pose2d(0, 0, Math.toRadians(0)))
+//                .lineToX(40);
+
+        waitForStart();
+
+        Actions.runBlocking(trajectory.build());
     }
 }
